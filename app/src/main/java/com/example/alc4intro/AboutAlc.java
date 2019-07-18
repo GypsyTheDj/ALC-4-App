1bc05fcd16454728ed5281cfe0f66ad5189c1249
Norman Nuthu <gypsythedj@gmail.com>
2019-07-18 15:15:16 +0300

    ALC 4.0 Android Challenge 1
    Master Activity Navigation and Support WebView in App.

--- app/src/main/java/com/example/alc4intro/AboutAlc.java
+++ app/src/main/java/com/example/alc4intro/AboutAlc.java
@@ -1,12 +1,9 @@
 package com.example.alc4intro;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.SslErrorHandler;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AboutAlc extends AppCompatActivity {
 
 
-    private static final String URL = "http://www.andela.com/alc/";
+    private String URL = "https://andela.com/alc";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,37 +21,21 @@ public class AboutAlc extends AppCompatActivity {
 
         WebView myWebView = findViewById(R.id.myWebView);
 
-        WebSettings settings = myWebView.getSettings();
-        myWebView.getSettings();
         myWebView.setWebViewClient(new MyWebViewClient());
-        settings.setDomStorageEnabled(true);
-        myWebView.getSettings().setAllowFileAccess( true );
-        myWebView.getSettings().setAppCacheEnabled( true );
+        myWebView.getSettings().setAllowFileAccess(true);
+        myWebView.getSettings().setAppCacheEnabled(true);
         myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         myWebView.getSettings().setUseWideViewPort(true);
         myWebView.getSettings().setLoadWithOverviewMode(true);
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+
         myWebView.loadUrl(URL);
     }
 
     private class MyWebViewClient extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals(URL)) {
-                return false;
-            }
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-            return true;
-        }
-
-        @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-            super.onReceivedSslError(view, handler, error);
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError er) {
             handler.proceed();
+            // Ignore SSL certificate errors
         }
     }
 }
-
-
